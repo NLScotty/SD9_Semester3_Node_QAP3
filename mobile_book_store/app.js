7a80fe36f3d2b7f0279d4eb376225499bc7f7b62
NLScotty <Scott.B.Normore@gmail.com>
2024-03-26 20:52:58 -0230

    programming done! just need to do documentation and video

--- mobile_book_store/app.js
+++ mobile_book_store/app.js
@@ -1,17 +1,34 @@
+/*
+    This is the driver application/file of my project. Running this file through
+    node will host the server that allows one to 
+*/
+
+// Node modules needed for this file
 const express = require("express");
+
+// Files that contain the different routes of my application
 const apiRoutes = require('./routes/api-routes');
 const uiRoutes = require('./routes/ui-routes');
 
-
+// we set app to express so we can call the express functions
 const app = express();
-
+// we set a port number
 const PORT =  3000;
 
+// debug stuff
 global.DEBUG = true;
+if(DEBUG){
+    app.use((request, response, next) => {
+        console.log(request.method+", "+request.url)
+        next()
+    })
+}
 
+// we set our view enginge, what kind of data we are expecting from our requests,
+// and a folder for our public resources (images, style css.)
 app.set('view engine', 'ejs');
-
 app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
 
 app.use('/', uiRoutes)
 
